17f865e09c7f7b30721475be3b10d31e3ea098f2
daniltribushko <105868780+daniltribushko@users.noreply.github.com>
2023-11-26 21:50:52 +0500

    Initial commit

---
+++ src/main/java/com/example/sbertech2023/SberTech2023Application.java
@@ -0,0 +1,13 @@
+package com.example.sbertech2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberTech2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberTech2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbertech2023/SberTech2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbertech2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberTech2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

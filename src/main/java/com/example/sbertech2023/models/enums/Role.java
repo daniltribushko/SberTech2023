61a2940435af95a6d23d6f03fe6190edc3d7896c
daniltribushko <105868780+daniltribushko@users.noreply.github.com>
2023-12-13 16:40:41 +0500

    Сделал переадресацию по ролям

--- src/main/java/com/example/sbertech2023/models/enums/Role.java
+++ src/main/java/com/example/sbertech2023/models/enums/Role.java
@@ -18,6 +18,23 @@ public enum Role {
         switch (name){
             case "USER" -> role = USER;
             case "ORGANIZATION" -> role = ORGANIZATION;
+            case "ADMIN" -> role = ADMIN;
+        }
+
+        return Optional.ofNullable(role);
+    }
+
+    /**
+     * Получение роли из строки без администратора
+     *
+     * @param name строковое название роли
+     * @return роль в типе Optional
+     */
+    public static Optional<Role> mapFromStringWithoutAdmin(String name){
+        Role role = null;
+        switch (name){
+            case "USER" -> role = USER;
+            case "ORGANIZATION" -> role = ORGANIZATION;
         }
 
         return Optional.ofNullable(role);
